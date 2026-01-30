beca1b9be304225ef1af1991d61166600369b3d1
John Ramus <ramusj@oregonstate.edu>
2026-01-29 20:52:32 -0800

    alarm working

--- alarm/main/alarm.c
+++ alarm/main/alarm.c
@@ -9,7 +9,7 @@
 #define DAC_CHANNEL         DAC_CHAN_0 // D 25
 #define BUTTON_PIN          GPIO_NUM_26 // D 26 push button
 #define FREQUENCY_HZ        440
-#define SAMPLE_COUNT        64   //samples per cycle
+#define SAMPLE_COUNT        32   //samples per cycle
 #define SAMPLE_RATE_HZ      (FREQUENCY_HZ * SAMPLE_COUNT) // 28160hz
 #define SAMPLE_PERIOD_US    (1000000 / SAMPLE_RATE_HZ)     
 
@@ -26,14 +26,10 @@ static dac_oneshot_handle_t dac_handle;
 // ramp 0 to 255
 
 static const uint8_t sawtooth_table[SAMPLE_COUNT] = {
-      0,   4,   8,  12,  16,  20,  24,  28,
-     32,  36,  40,  44,  48,  52,  56,  60,
-     64,  68,  72,  76,  80,  84,  88,  92,
-     96, 100, 104, 108, 112, 116, 120, 124,
-    128, 132, 136, 140, 144, 148, 152, 156,
-    160, 164, 168, 172, 176, 180, 184, 188,
-    192, 196, 200, 204, 208, 212, 216, 220,
-    224, 228, 232, 236, 240, 244, 248, 252
+      0,   8,  16,  24,  32,  40,  48,  56,
+     64,  72,  80,  88,  96, 104, 112, 120,
+    128, 136, 144, 152, 160, 168, 176, 184,
+    192, 200, 208, 216, 224, 232, 240, 248
 };
 
 
@@ -57,11 +53,11 @@ static void button_task(void* arg)
     bool last_button_state = true;     //pullup = unpressed = high
 
     while (1) {
+
         bool current_button_state = gpio_get_level(BUTTON_PIN);
 
         if (last_button_state && !current_button_state) {
             tone_enabled = !tone_enabled;
-            printf("Tone %s\n", tone_enabled ? "ON" : "OFF");
 
             vTaskDelay(pdMS_TO_TICKS(50));   // debounce delay
         }
